91964698963944a05640dcdada296b93b65c6982
Suyash Gupta <suyashbetu@gmail.com>
2022-01-05 00:21:18 +0530

    Delete profile directory

--- profile/src/main/java/com/peerlending/profile/domain/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.peerlending.profile.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.peerlending.profile.domain.model.User;
-
-public interface UserRepository extends JpaRepository<User,String> {
-
-}
